f955f7286e5197dee8c43b918338ed426034b336
Precious Humphrey <phumphrey23@gmail.com>
2018-07-28 20:55:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
     student = {}
-    links = profile_page.css(".social-icon-container").children.css("a").map { |element| element.attribute('href').value}
+    links = profile_page.css(".social-icon-container").children.css("a").map {|element| element.attribute('href').value}
     links.each do |link|
       if link.include?("twitter")
         student[:twitter] = link
