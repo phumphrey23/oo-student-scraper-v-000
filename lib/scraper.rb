7cea4532c0b08f7c84fa03ac609bbd7cc52df737
Precious Humphrey <phumphrey23@gmail.com>
2018-07-28 20:55:03 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,6 +34,7 @@ class Scraper
       student[:profile_quote] = profile_page.css(".profile-quote").text
       student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text
     end
+  end
 
       #student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
       #student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css(".description-holder p")
